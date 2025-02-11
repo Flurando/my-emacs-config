003636c8ffb19bc276a4e870a5ff785a58f97ebc
Jack <2024110144@mails.szu.edu.cn>
2025-02-11 19:22:59 +0800

    huge change in an attempt of using use-package

--- exwm/exwm.el
+++ exwm/exwm.el
@@ -1,77 +1,104 @@
 ;;;exwm
-(require 'exwm)
-(setq exwm-workspace-number 2)
-(add-hook 'exwm-update-class-hook
-	  (lambda ()
-	    (unless (or (string-prefix-p "sun-awt-X11-" exwm-instance-name)
+(use-package exwm
+  :ensure t
+  :config
+  (setq exwm-workspace-number 2)
+  (add-hook 'exwm-update-class-hook
+	    (lambda ()
+	      (unless (or (string-prefix-p "sun-awt-X11-" exwm-instance-name)
+			  (string= "gimp" exwm-instance-name))
+		(exwm-workspace-rename-buffer exwm-class-name))))
+  (add-hook 'exwm-update-title-hook
+	    (lambda ()
+	      (when (or (not exwm-instance-name)
+			(string-prefix-p "sun-awt-X11-" exwm-instance-name)
 			(string= "gimp" exwm-instance-name))
-	      (exwm-workspace-rename-buffer exwm-class-name))))
-(add-hook 'exwm-update-title-hook
-	  (lambda ()
-	    (when (or (not exwm-instance-name)
-		      (string-prefix-p "sun-awt-X11-" exwm-instance-name)
-		      (string= "gimp" exwm-instance-name))
-	      (exwm-workspace-rename-buffer exwm-title))))
-(setq exwm-input-global-keys
-      `(([?\s-r] . exwm-reset) ;; s-r: Reset (to line-mode).
-	([?\s-w] . exwm-workspace-switch) ;; s-w: Switch workspace.
-	([?\s-`] . (lambda ()
-		     (interactive)
-		     (exwm-workspace-switch-create 0)))
-	([?\s-&] . (lambda (cmd) ;; s-&: Launch application.
-		     (interactive (list (read-shell-command "$ ")))
-		     (start-process-shell-command cmd nil cmd)))
-	,@(mapcar (lambda (i)
-		    `(,(kbd (format "s-%d" i)) .
-		      (lambda ()
-			(interactive)
-			(exwm-workspace-switch-create ,i))))
-		  (number-sequence 0 9))
-	;;Move between windows
-	([s-left] . windmove-left)
-	([s-right] . windmove-right)
-	([s-up] . windmove-up)
-	([s-down] . windmove-down)
-	;;Support quick switch line/char mode in gui apps
-	([?\s-o] . exwm-input-toggle-keyboard)))
+		(exwm-workspace-rename-buffer exwm-title))))
+  (setq exwm-input-global-keys
+	`(([?\s-r] . exwm-reset) ;; s-r: Reset (to line-mode).
+	  ([?\s-w] . exwm-workspace-switch) ;; s-w: Switch workspace.
+	  ([?\s-`] . (lambda ()
+		       (interactive)
+		       (exwm-workspace-switch-create 0)))
+	  ([?\s-&] . (lambda (cmd) ;; s-&: Launch application.
+		       (interactive (list (read-shell-command "$ ")))
+		       (start-process-shell-command cmd nil cmd)))
+	  ,@(mapcar (lambda (i)
+		      `(,(kbd (format "s-%d" i)) .
+			(lambda ()
+			  (interactive)
+			  (exwm-workspace-switch-create ,i))))
+		    (number-sequence 0 9))
+	  ;;Move between windows
+	  ([s-left] . windmove-left)
+	  ([s-right] . windmove-right)
+	  ([s-up] . windmove-up)
+	  ([s-down] . windmove-down)
+	  ;;Support quick switch line/char mode in gui apps
+	  ([?\s-o] . exwm-input-toggle-keyboard)))
 
-(setq exwm-input-simulation-keys
-      '(
-	;; movement
-	([?\C-b] . [left])
-	([?\M-b] . [C-left])
-	([?\C-f] . [right])
-	([?\M-f] . [C-right])
-	([?\C-p] . [up])
-	([?\C-n] . [down])
-	([?\C-a] . [home])
-	([?\C-e] . [end])
-	([?\M-v] . [prior])
-	([?\C-v] . [next])
-	([?\C-d] . [delete])
-	([?\C-k] . [S-end delete])
-	;; cut/paste
-	([?\C-w] . [?\C-x])
-	([?\M-w] . [?\C-c])
-	([?\C-y] . [?\C-v])
-	;; search
-	([?\C-s] . [?\C-f])))
-(setq exwm-input-prefix-keys
-      '(?\C-x
-	?\C-u
-	?\C-h
-	?\M-x
-	?\M-`
-	?\M-:
-	?\C-\M-j ; Buffer list
-	?\C-\ )) ; Ctrl+Space
+  (setq exwm-input-simulation-keys
+	'(
+	  ;; movement
+	  ([?\C-b] . [left])
+	  ([?\M-b] . [C-left])
+	  ([?\C-f] . [right])
+	  ([?\M-f] . [C-right])
+	  ([?\C-p] . [up])
+	  ([?\C-n] . [down])
+	  ([?\C-a] . [home])
+	  ([?\C-e] . [end])
+	  ([?\M-v] . [prior])
+	  ([?\C-v] . [next])
+	  ([?\C-d] . [delete])
+	  ([?\C-k] . [S-end delete])
+	  ;; cut/paste
+	  ([?\C-w] . [?\C-x])
+	  ([?\M-w] . [?\C-c])
+	  ([?\C-y] . [?\C-v])
+	  ;; search
+	  ([?\C-s] . [?\C-f])))
+  (setq exwm-input-prefix-keys
+	'(?\C-x
+	  ?\C-u
+	  ?\C-h
+	  ?\M-x
+	  ?\M-`
+	  ?\M-:
+	  ?\C-\M-j ; Buffer list
+	  ?\C-\ )) ; Ctrl+Space
 
-;; Ctrl+Q will enable the next key to be sent directly
-(define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
+  ;; Ctrl+Q will enable the next key to be sent directly
+  (define-key exwm-mode-map [?\C-q] 'exwm-input-send-next-key)
 
-;; You can hide the minibuffer and echo area when they're not used, by uncommenting the following line.
-;(setq exwm-workspace-minibuffer-position 'bottom)
+  ;; You can hide the minibuffer and echo area when they're not used, by uncommenting the following line.
+  ;;(setq exwm-workspace-minibuffer-position 'bottom)
 
-;; simple system tray
-(require 'exwm-systemtray)
-(exwm-systemtray-mode 1)
+  ;; For counsel-linux-app
+  (exwm-input-set-key (kbd "s-SPC") 'counsel-linux-app)
+  (exwm-input-set-key (kbd "s-f") 'exwm-layout-toggle-fullscreen)
+  
+  ;; simple system tray
+  (require 'exwm-systemtray)
+  (exwm-systemtray-mode 1))
+
+;; Allow resizing with mouse, of non-floating windows.
+(use-package window-divider
+  :ensure t
+  :after exwm
+  :init
+  (setq window-divider-default-bottom-width 2
+	window-divider-default-right-width 2)
+  :config
+  (window-divider-mode))
+
+(use-package desktop-environment
+  :ensure t
+  :ensure-system-package (scrot brightnessctl playerctl)
+  :after exwm
+  :config (desktop-environment-mode)
+  :custom
+  (desktop-environment-brightness-small-increment "2%+")
+  (desktop-environment-brightness-small-decrement "2%-")
+  (desktop-environment-brightness-normal-increment "5%+")
+  (desktop-environment-brightness-normal-decrement "5%-"))
--- init.el
+++ init.el
@@ -1,52 +1,132 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-by-copying-when-linked t)
+ '(display-time-format "[%Y.%m.%d-%H:%M:%S(%Z)]")
+ '(display-time-mode nil)
+ '(erc-modules
+   '(autojoin button completion fill irccontrols list match menu move-to-prompt netsplit networks noncommands readonly ring sasl stamp track))
+ '(fringe-mode 1 nil (fringe))
+ '(icomplete-vertical-mode t)
+ '(ido-mode nil nil (ido))
+ '(inferior-lisp-program "sbcl")
+ '(inhibit-startup-screen t)
+ '(initial-major-mode 'fundamental-mode)
+ '(initial-scratch-message
+   "Make it happen, or let it go.\12Life is simple like a show.\12\12Face the darkest, or hide the fist.\12Magic swarms in the mist.\12\12Never mind what being met.\12Only weep for the regret.\12\12Please insist till the last.\12Follow the call from the past.\12\12")
+ '(ivy-mode t)
+ '(menu-bar-mode nil)
+ '(mode-line-percent-position nil)
+ '(mouse-wheel-down-alternate-event 'wheel-down)
+ '(mouse-wheel-down-event 'mouse-5)
+ '(mouse-wheel-tilt-scroll t)
+ '(mouse-wheel-up-alternate-event 'wheel-up)
+ '(mouse-wheel-up-event 'mouse-4)
+ '(package-archives
+   '(("gnu" . "https://elpa.gnu.org/packages/")
+     ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+     ("melpa" . "https://melpa.org/packages/")))
+ '(package-enable-at-startup nil)
+ '(package-install-upgrade-built-in t)
+ '(package-selected-packages
+   '(compat magit guix sly system-packages geiser wesnoth-mode quelpa-use-package quelpa cdlatex geiser-guile))
+ '(scheme-program-name "guile")
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "yellow" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 300 :width normal :foundry "GOOG" :family "Noto Sans Mono"))))
+ '(mode-line ((t (:background "black" :foreground "gold" :box (:line-width (1 . -1) :style released-button)))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "grey30" :foreground "grey80" :box (:line-width (1 . -1) :color "grey40") :weight light)))))
 
-(setq inferior-lisp-program (executable-find "sbcl"))
-(setq sly-lisp-implementations
-      '((sbcl ("sbcl"))
-	(clisp ("clisp"))))
+;; Actually use-package is available by default in newer emacs, but whatever!
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(use-package use-package-ensure-system-package)
 
-;;Wesnoth WML mode
-(add-to-list 'load-path "~/apps/wesnoth-mode/")
-(autoload 'wesnoth-mode "wesnoth-mode" "Major mode for editing WML." t)
+;; Enable :ensure-system-package in use-package
+;; by default, it uses guix to install not available stuff
+;; pass string for other cases
+(use-package system-packages :ensure t)
 
-;;scheme lsp
-;;sadly not really usable, wait until it enters default guix channel
-;(require 'eglot)
-;(add-to-list 'eglot-server-programs
-;	     `(scheme-mode . ("guile-lsp-server")))
-;(add-hook 'scheme-mode-hook #'eglot-ensure) ;uncomment this line only when automatic start is desired, but to my test the guile-lsp-server is doing nothing about enhancing geiser, only making life harder unfortunately
-(setq scheme-program-name "guile")
+;; Set up quelpa and enable :quelpa in use-package
+;; Caution: don't :ensure t and :quelpa (...) at the same time
+(use-package quelpa
+  :ensure t
+  :custom
+  (quelpa-update-melpa-p nil))
+(use-package quelpa-use-package :ensure t)
 
-;;change *scratch* content
-(setq initial-scratch-message
-"Make it happen, or let it go.
-Life is simple like a show.
+;; Some stuff required when guix and melpa is conflicting
+(use-package compat :ensure t)
+(use-package dash :ensure t)
 
-Face the darkest, or hide the fist.
-Magic swarms in the mist.
+;; Wesnoth WML mode
+(use-package wesnoth-mode
+  :ensure-system-package git
+  :quelpa
+  (wesnoth-mode :fetcher git
+		:url "git://repo.or.cz/wesnoth-mode.git"
+		:build (:not compile))
+  :config
+  (autoload 'wesnoth-mode "wesnoth-mode" "Major mode for editing WML." t))
 
-Never mind what being met.
-Only weep for the regret.
+(use-package eshell
+  :init
+  (setq eshell-banner-message ""))
 
-Please insist till the last.
-Follow the call from the past.
+;; Sly, the must-have for commonlisp work in emacs
+;; Caution: up to my experience, sbcl installed by guix is faulty in a manner that gnome might be broken by it -- some pointed out that changing some environment variables like XDG_DATA_DIRS manually can fix, but that is not true for me and I recommend you to check the issue yourself at bug-guix bug#45360 -- not fixed and nearly 0 hope of being fixed in the future. You might want to avoid that by hand! Just in case, if one happens to have guix available but no sbcl installed already.
+(use-package sly
+  :ensure t
+  :ensure-system-package sbcl)
+;;(use-package sly-quicklisp :ensure t :after sly)
+;;(use-package sly-named-readtables :ensure t :after sly)
+;;(use-package sly-macrostep :ensure t :after sly)
+;;(use-package sly-stepper :ensure t :after sly)
+;;(use-package sly-asdf :ensure t :after sly)
+;;(use-package sly-named-readtables :ensure t :after sly)
 
-")
+;; emacs-guix, the guix interface
+(use-package guix
+  :ensure t
+  :ensure-system-package guix)
 
-;;use ivy-mode instead of ido-mode
-(ido-mode nil)
-(ivy-mode 1)
+;; Geiser, the slime/sly (Common Lisp) equivalence in Scheme world
+(use-package geiser
+  :ensure t
+  :ensure-system-package guile
+  :custom
+  (geiser-active-implementations '(guile)))
+(use-package geiser-guile
+  :ensure t
+  :ensure-system-package guile
+  :after geiser
+  :custom
+  (geiser-guile-binary (executable-find "guile"))
+  (geiser-guile-load-path nil)
+  (geiser-guile-load-init-file t))
 
-;; Allow resizing with mouse, of non-floating windows.
-(setq window-divider-default-bottom-width 2
-      window-divider-default-right-width 2)
-(window-divider-mode)
+;; Auto completion
+(use-package ivy
+  :ensure t)
+(use-package swiper
+  :ensure t
+  :after ivy)
+(use-package counsel
+  :ensure t
+  :after (:all ivy swiper)
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only))
 
-;; erc related confs, currently only login to libera chat with identity is supported
-(setopt erc-sasl-mechanism 'plain)
-(load "~/.emacs.d/erc/connect-spec-proc.el")
+;; Magit, the git interface for emacs
+(use-package magit
+  :ensure t
+  :ensure-system-package git)
